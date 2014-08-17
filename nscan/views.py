9fe2310eeddd85898518c35288c0a41de6ce358b
Gerasimos Melissaratos <gmelis@nia0.room.gr>
2014-08-17 23:23:40 +0300

    a

---
+++ nscan/views.py
@@ -0,0 +1,10 @@
+#from django.shortcuts import render
+from django.template import RequestContext, Context, loader
+from django.shortcuts import render_to_response, render
+from django.shortcuts import get_object_or_404
+from django.shortcuts import get_list_or_404
+
+# Create your views here.
+
+def scan(request):
+    return render_to_response('nscan/index.html', context_instance = RequestContext(request))
