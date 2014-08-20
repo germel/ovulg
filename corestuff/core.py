b637b7690ba5d6273abb63e72e64c0b65596bd93
Gerasimos Melissaratos <gmelis@thalassa.mikroskosmos.gr>
2014-08-20 18:56:03 +0300

    2014082001

---
+++ corestuff/core.py
@@ -0,0 +1,32 @@
+def CiscoScan(device, snmp_comm, snmp_pass):
+    from pysnmp.entity.rfc3413.oneliner import cmdgen
+    
+    if snmp_comm:
+        community = snmp_comm
+    else:
+        community = 'public'
+    
+    cmdGen = cmdgen.CommandGenerator()
+    
+    errorIndication, errorStatus, errorIndex, varBindTable = cmdGen.bulkCmd(
+        cmdgen.CommunityData(community),
+        cmdgen.UdpTransportTarget((device, 161)),
+        0, 1,
+        '1.3.6.1.4.1.9.9.23.1.2.1.1.4',
+        '1.3.6.1.4.1.9.9.23.1.2.1.1.6',
+        '1.3.6.1.4.1.9.9.23.1.2.1.1.7'
+    )
+    
+    # Check for errors and print out results
+    if errorIndication:
+        return(errorIndication)
+    else:
+        if errorStatus:
+            return(
+                print('%s at %s' % (
+                errorStatus.prettyPrint(),
+                errorIndex and varBinds[int(errorIndex)-1] or '?')
+                )
+            )
+        else:
+            return(varBindTable)
--- nscan/views.py
+++ nscan/views.py
@@ -4,15 +4,23 @@ from django.shortcuts import get_object_or_404
 from django.shortcuts import get_list_or_404
 
 from nscan.forms import SwitchForm
+from corestuff.core import CiscoScan
 # Create your views here.
 
     
 def scan(request):
     if request.method == 'POST':
         form = SwitchForm(request.POST)
-        #if form.isvalid():
-        return HttpResponse('/thanks/')
-        #pass
+        if form.is_valid():
+            s_ip = form.cleaned_data['switch_name']
+            s_comm = form.cleaned_data['snmp_community']
+            s_pass = form.cleaned_data['snmp_pass']
+            resp = CiscoScan(s_ip, s_comm, s_pass)
+            return HttpResponse(resp)
+        else:
+            return HttpResponse('/no, thanks/')
+        pass
     else:
         return render_to_response('scan.html', {'form': SwitchForm()}, context_instance = RequestContext(request))
         #return render(request, 'scan.html', {'form': ExampleForm()})
+
