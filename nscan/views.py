522c6317a7bc30e13bfaf5bae085c091b1df61af
Gerasimos Melissaratos <gmelis@thalassa.mikroskosmos.gr>
2014-08-24 11:22:01 +0300

    2014082400

--- nscan/views.py
+++ nscan/views.py
@@ -35,4 +35,15 @@ def scan(request):
             return HttpResponse('Go back and fill that form like a good girl...')
         pass
     else:
-        return render(request, 'scan.html', {'form': SwitchForm()})
+        x='''<script type="text/javascript>
+            var rectDemo = d3.select("#rect-demo")
+                .append("svg:svg")
+                .attr("width", 400)
+                .attr("height", 300);
+            rectDemo.append("svg:rect")
+                .attr("x", 100)
+                .attr("y", 100)
+                .attr("height", 100)
+                .attr("width", 200);
+            </script>'''
+        return render(request, 'scan.html', {'form': SwitchForm(), 'x': x})
