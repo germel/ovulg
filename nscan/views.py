d622edd8a886f6bf960907f33c459f870a13c8ce
Gerasimos Melissaratos <gmelis@thalassa.mikroskosmos.gr>
2014-12-17 21:25:32 +0200

    2014121700

--- nscan/views.py
+++ nscan/views.py
@@ -118,4 +118,4 @@ def sigmafy(data):
     #sdata = sdata.replace('"label":"172.16.40.1"', '"label":"172.16.40.1","id":"n0","size":10,"x":1,"y":1')
     #sdata = '{"nodes": [{"label":"172.16.40.1","id":"n0","size":"6","x":"1","y":"1"}]}'
     #sdata = '{ "nodes": [ { "id": "n0", "label": "A node", "x": 0, "y": 0, "size": 3 }, { "id": "n1", "label": "Another node", "x": 3, "y": 1, "size": 2 }, { "id": "n2", "label": "And a last one", "x": 1, "y": 3, "size": 1 } ], "edges": [ { "id": "e0", "source": "n0", "target": "n1" }, { "id": "e1", "source": "n1", "target": "n2" }, { "id": "e2", "source": "n2", "target": "n0" } ] }'
-    return sdata
+    return data
